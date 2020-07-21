9bac9e5b61a37ae476a78b033627518fdc45e340
KRISHNA KANT SINGH <krishnakant@ug.cusat.ac.in>
2020-07-22 01:15:30 +0530

    Update 15_fifo.c

--- 15_fifo.c
+++ 15_fifo.c
@@ -2,16 +2,16 @@
 int main()
 {
 int i, j, k, f, pf=0, count=0, rs[25], m[10], n;
-printf("\n Enter the length of reference string -- ");
-scanf("%d",&n);
-printf("\n Enter the reference string -- ");
+	printf("\n Enter the length of reference string -- ");
+	scanf("%d",&n);
+	printf("\n Enter the reference string -- ");
 for(i=0;i<n;i++)
-scanf("%d",&rs[i]);
-printf("\n Enter no. of frames -- ");
-scanf("%d",&f);
+	scanf("%d",&rs[i]);
+	printf("\n Enter no. of frames -- ");
+	scanf("%d",&f);
 for(i=0;i<f;i++)
-m[i]=-1;
-printf("\n The Page Replacement Process is -- \n");
+	m[i]=-1;
+	printf("\n The Page Replacement Process is -- \n");
 for(i=0;i<n;i++)
 {
 for(k=0;k<f;k++)
